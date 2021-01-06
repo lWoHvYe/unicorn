51ab228f51c40069fd3137227498b60c2454de8b
lWoHvYe <39266195+lWoHvYe@users.noreply.github.com>
2021-01-06 13:44:21 +0800

    Java 15开始无法使用工具类的算术验证码。改用gif。部分依赖升级

--- eladmin-system/src/main/java/com/lwohvye/modules/security/config/bean/LoginProperties.java
+++ eladmin-system/src/main/java/com/lwohvye/modules/security/config/bean/LoginProperties.java
@@ -77,6 +77,7 @@ public class LoginProperties {
         Captcha captcha;
         synchronized (this) {
             switch (loginCode.getCodeType()) {
+                // jdk15开始无法使用算术验证码
                 case arithmetic:
                     // 算术类型 https://gitee.com/whvse/EasyCaptcha
                     captcha = new ArithmeticCaptcha(loginCode.getWidth(), loginCode.getHeight());
