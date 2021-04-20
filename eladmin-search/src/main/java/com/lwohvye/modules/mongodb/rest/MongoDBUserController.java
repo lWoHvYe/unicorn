5e087ad6fcb4560dab923e589eb657e4f82fd928
lWoHvYe <lWoHvYe@outlook.com>
2021-04-20 17:57:39 +0800

    SPI指定实现的Bean。但简单但配置。无法注入bean需要但属性 Spring SpringFactoriesLoader版

--- eladmin-search/src/main/java/com/lwohvye/modules/mongodb/rest/MongoDBUserController.java
+++ eladmin-search/src/main/java/com/lwohvye/modules/mongodb/rest/MongoDBUserController.java
@@ -2,8 +2,10 @@ package com.lwohvye.modules.mongodb.rest;
 
 import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.util.ObjectUtil;
+import cn.hutool.core.util.RandomUtil;
 import com.lwohvye.annotation.AnonymousAccess;
 import com.lwohvye.modules.mongodb.service.MongoDBUserService;
+import org.springframework.core.io.support.SpringFactoriesLoader;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,11 +28,22 @@ public class MongoDBUserController {
 
     @PostConstruct
     private void doInit() {
-        var mongoDBUserServices = ServiceLoader.load(MongoDBUserService.class);
+        if (ObjectUtil.isNull(mongoDBUserService)) {
+//            SPI的两种方式：Java ServiceLoader和Spring SpringFactoriesLoader。当前都无法注入属性。待解决
+            if (RandomUtil.randomBoolean()) {
+                var mongoDBUserServices = ServiceLoader.load(MongoDBUserService.class);
 //        有值，且大小唯一
-        if (ObjectUtil.isNotNull(mongoDBUserServices) && CollUtil.isNotEmpty(mongoDBUserServices) && ObjectUtil.equal(mongoDBUserServices.stream().count(), 1L)) {
-            var optionalMongoDBUserService = mongoDBUserServices.findFirst();
-            optionalMongoDBUserService.ifPresent(dbUserService -> this.mongoDBUserService = dbUserService);
+                if (ObjectUtil.isNotNull(mongoDBUserServices) && CollUtil.isNotEmpty(mongoDBUserServices) && ObjectUtil.equal(mongoDBUserServices.stream().count(), 1L)) {
+                    var optionalMongoDBUserService = mongoDBUserServices.findFirst();
+                    optionalMongoDBUserService.ifPresent(dbUserService -> this.mongoDBUserService = dbUserService);
+                }
+            } else {
+                var mongoDBUserServices = SpringFactoriesLoader.loadFactories(MongoDBUserService.class, null);
+//        有值，且大小唯一
+                if (ObjectUtil.isNotNull(mongoDBUserServices) && CollUtil.isNotEmpty(mongoDBUserServices) && ObjectUtil.equal(mongoDBUserServices.size(), 1)) {
+                    this.mongoDBUserService = mongoDBUserServices.get(0);
+                }
+            }
         }
     }
 
