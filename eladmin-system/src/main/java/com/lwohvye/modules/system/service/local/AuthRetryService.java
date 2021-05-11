429567622400891ddd5e7c272d764e9b5bcc5439
lWoHvYe <lWoHvYe@outlook.com>
2021-05-11 23:44:36 +0800

    Spring Retry相关示例，部分注释调整

--- eladmin-system/src/main/java/com/lwohvye/modules/system/service/local/AuthRetryService.java
+++ eladmin-system/src/main/java/com/lwohvye/modules/system/service/local/AuthRetryService.java
@@ -15,6 +15,16 @@ public class AuthRetryService {
      * @description
      * @EnableRetry – 表示开启重试机制
      * @Retryable – 表示这个方法需要重试，它有很丰富的参数，可以满足你对重试的需求
+     *      recover：指定兜底/补偿的方法名。如果不指定，默认对照 @Recover 标识的，第一入参为重试异常，其余入参和出参一致的方法；
+     *      interceptor：指定方法切面 bean， org.aopalliance.intercept.MethodInterceptor 实现类
+     *      value / include：两者用途一致，指出哪些类型需要重试；
+     *      exclude：和 include 相反，指出哪些异常不需要重试；
+     *      label：可以指定唯一标签，用于统计；
+     *      stateful：默认false。重试是否是有状态的；
+     *      maxAttempts：最大的重试次数；
+     *      backoff：指定 @Backoff ，回退策略；
+     *      listeners：指定 org.springframework.retry.RetryListener 实现 bean。
+     *  重试策略
      * SimpleRetryPolicy 默认最多重试3次
      * TimeoutRetryPolicy 默认在1秒内失败都会重试
      * ExpressionRetryPolicy 符合表达式就会重试
@@ -23,6 +33,11 @@ public class AuthRetryService {
      * NeverRetryPolicy 从不重试（也是一种重试策略哈）
      * AlwaysRetryPolicy 总是重试
      * @Backoff – 表示重试中的退避策略
+     *      value / delay：两者都标识延迟时间，为 0则对应 NoBackOffPolicy 策略。
+     *      maxDelay：最大延迟时间
+     *      multiplier：递增乘数
+     *      random：递增乘数是否随机
+     *  退避策略
      * FixedBackOffPolicy 默认固定延迟1秒后执行下一次重试
      * ExponentialBackOffPolicy 指数递增延迟执行重试，默认初始0.1秒，系数是2，那么下次延迟0.2秒，再下次就是延迟0.4秒，如此类推，最大30秒。
      * ExponentialRandomBackOffPolicy 在上面那个策略上增加随机性
@@ -35,15 +50,15 @@ public class AuthRetryService {
      * @date 2021/4/23 1:13 下午
      */
     @Retryable(value = IllegalAccessException.class, maxAttempts = 5,
-            backoff = @Backoff(value = 1500, maxDelay = 100000, multiplier = 1.2))
-    public void service() throws IllegalAccessException {
-        System.out.println("service method...");
+            backoff = @Backoff(value = 1500, maxDelay = 100000, multiplier = 1.2, random = true))
+    public void retryService() throws IllegalAccessException {
+        log.info("service method...");
         if (RandomUtil.randomBoolean())
             throw new IllegalAccessException("manual exception");
     }
 
     @Recover
     public void recover(IllegalAccessException e) {
-        System.out.println("service retry after Recover => " + e.getMessage());
+        log.error("service retry after Recover => {}", e.getMessage());
     }
 }
