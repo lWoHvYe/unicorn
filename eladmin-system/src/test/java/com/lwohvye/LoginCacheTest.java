ae424ec154982527112b8ccd819807886591fb00
lWoHvYe <lWoHvYe@outlook.com>
2021-10-28 07:44:34 +0800

    ListNode添加一个有参构造

--- eladmin-system/src/test/java/com/lwohvye/LoginCacheTest.java
+++ eladmin-system/src/test/java/com/lwohvye/LoginCacheTest.java
@@ -73,6 +73,11 @@ class ListNode {
     ListNode(int val) {
         this.val = val;
     }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
 }
 
 class TreeNode {
