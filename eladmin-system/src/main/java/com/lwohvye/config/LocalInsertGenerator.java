2eb0a502874608b22e7356febc4e46c0aa6c275b
lWoHvYe <lWoHvYe@outlook.com>
2021-11-08 16:58:10 +0800

    LICENSE

--- eladmin-system/src/main/java/com/lwohvye/config/LocalInsertGenerator.java
+++ eladmin-system/src/main/java/com/lwohvye/config/LocalInsertGenerator.java
@@ -1,3 +1,18 @@
+/*
+ *  Copyright 2020-2022 lWoHvYe(Hongyan Wang)
+ *  <p>
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  <p>
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *  <p>
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package com.lwohvye.config;
 
 import org.hibernate.HibernateException;
@@ -7,10 +22,9 @@ import org.hibernate.id.IdentityGenerator;
 import java.io.Serializable;
 
 /**
- *  自定义的主键生成策略，如果填写了主键id，如果数据库中没有这条记录，则新增指定id的记录；否则更新记录
- *
- *  如果不填写主键id，则利用数据库本身的自增策略指定id
- *
+ * 自定义的主键生成策略，如果填写了主键id，如果数据库中没有这条记录，则新增指定id的记录；否则更新记录
+ * <p>
+ * 如果不填写主键id，则利用数据库本身的自增策略指定id
  */
 public class LocalInsertGenerator extends IdentityGenerator {
 
