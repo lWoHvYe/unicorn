317e59dd8eb878034c3fab1c7228c6c1e022a7c0
lWoHvYe <lWoHvYe@outlook.com>
2021-11-29 11:39:52 +0800

    package-info.java https://www.lwohvye.com/2021/11/29/java%e9%a1%b9%e7%9b%ae%e4%b8%ad%e7%9a%84package-info-java%e7%9a%84%e4%bd%9c%e7%94%a8/

---
+++ eladmin-system/src/main/java/com/lwohvye/modules/system/handler/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * Provides basic classes for exception handling and version detection,
+ * and other core helpers that are not specific to any part of the framework.
+ * <a href="https://www.lwohvye.com/2021/11/29/java%e9%a1%b9%e7%9b%ae%e4%b8%ad%e7%9a%84package-info-java%e7%9a%84%e4%bd%9c%e7%94%a8/">package-info.java</a>
+ */
+//@NonNullApi，包（Package）级别注解，指定参数和方法返回值默认不能为null；
+//@NonNullFields，包（Package）级别注解，用于变量不能为null；
+@NonNullApi
+@NonNullFields
+package com.lwohvye.modules.system.handler;
+
+import org.springframework.lang.NonNullApi;
+import org.springframework.lang.NonNullFields;
