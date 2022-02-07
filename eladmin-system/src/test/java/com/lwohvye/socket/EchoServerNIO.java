6ae800074cbc54cbe83f155e6c970b9f5d610cbf
lWoHvYe <lWoHvYe@outlook.com>
2022-02-07 10:52:08 +0800

    NIOSocket添加部分注释

--- eladmin-system/src/test/java/com/lwohvye/socket/EchoServerNIO.java
+++ eladmin-system/src/test/java/com/lwohvye/socket/EchoServerNIO.java
@@ -26,7 +26,7 @@ import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.util.Iterator;
 
-// 用 NIO 的多路复用套接字实现服务器，Socket监听回显，不是很容易理解
+// 用 NIO 的多路复用套接字实现服务器，Socket监听回显
 public class EchoServerNIO {
     private static final int ECHO_SERVER_PORT = 6789;
     private static final int ECHO_SERVER_TIMEOUT = 5000;
@@ -41,12 +41,14 @@ public class EchoServerNIO {
 
     private static void init() {
         try {
-            ServerSocketChannel serverChannel = ServerSocketChannel.open();
-            buffer = ByteBuffer.allocate(BUFFER_SIZE);
-            serverChannel.socket().bind(new InetSocketAddress(ECHO_SERVER_PORT));
-            serverChannel.configureBlocking(false);
-            selector = Selector.open();
-            serverChannel.register(selector, SelectionKey.OP_ACCEPT);
+            ServerSocketChannel serverChannel = ServerSocketChannel.open(); // 创建通道
+            buffer = ByteBuffer.allocate(BUFFER_SIZE); // 读写缓冲区
+            serverChannel.socket().bind(new InetSocketAddress(ECHO_SERVER_PORT)); // 绑定
+            serverChannel.configureBlocking(false); // 通道需设置为非阻塞
+
+            selector = Selector.open(); // 创建选择器
+
+            serverChannel.register(selector, SelectionKey.OP_ACCEPT); // 将通道注册到选择器上
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
@@ -55,12 +57,12 @@ public class EchoServerNIO {
     private static void listen() {
         while (true) {
             try {
-                if (selector.select(ECHO_SERVER_TIMEOUT) != 0) {
-                    Iterator<SelectionKey> it = selector.selectedKeys().iterator();
-                    while (it.hasNext()) {
-                        SelectionKey key = it.next();
-                        it.remove();
-                        handleKey(key);
+                if (selector.select(ECHO_SERVER_TIMEOUT) != 0) { // 使用 select() 来监听到达的事件，它会一直阻塞直到有至少一个事件到达。
+                    Iterator<SelectionKey> keyIterator = selector.selectedKeys().iterator(); // 获取到达的事件
+                    while (keyIterator.hasNext()) {
+                        SelectionKey key = keyIterator.next();
+                        handleKey(key); // 处理事件
+                        keyIterator.remove(); // 处理完后移除事件
                     }
                 }
             } catch (Exception e) {
@@ -74,18 +76,21 @@ public class EchoServerNIO {
         try {
             if (key.isAcceptable()) {
                 ServerSocketChannel serverChannel = (ServerSocketChannel) key.channel();
+                // 服务器会为每个新连接创建一个 SocketChannel
                 channel = serverChannel.accept();
                 channel.configureBlocking(false);
+                // 这个新连接主要用于从客户端读取数据
                 channel.register(selector, SelectionKey.OP_READ);
             } else if (key.isReadable()) {
                 channel = (SocketChannel) key.channel();
                 buffer.clear();
-                if (channel.read(buffer) > 0) {
-                    buffer.flip();
+                if (channel.read(buffer) != -1) {
+                    buffer.flip(); // 读写模式切换
                     CharBuffer charBuffer = CharsetHelper.decode(buffer);
                     String msg = charBuffer.toString();
                     System.out.println("收到" + channel.getRemoteAddress() + "的消息:" + msg);
                     channel.write(CharsetHelper.encode(CharBuffer.wrap(msg)));
+                    buffer.clear(); // 写完清空
                 } else {
                     channel.close();
                 }
