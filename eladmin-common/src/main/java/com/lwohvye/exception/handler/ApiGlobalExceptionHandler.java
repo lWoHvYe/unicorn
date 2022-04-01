cd1dbfe48e43e55f2d82442eedb3e79f3b2fd698
lWoHvYe <lWoHvYe@outlook.com>
2022-04-01 08:53:20 +0800

    移除已过期的类。错误修复

--- eladmin-common/src/main/java/com/lwohvye/exception/handler/ApiGlobalExceptionHandler.java
+++ eladmin-common/src/main/java/com/lwohvye/exception/handler/ApiGlobalExceptionHandler.java
@@ -39,7 +39,7 @@ import java.util.Objects;
 
 
 /**
- * 全局异常处理，替代原异常处理类{@link GlobalExceptionHandler}
+ * 全局异常处理，
  * 统一异常处理，因为已在SpringSecurityConfig里配置了401和403的handler，所以鉴权中的，401和403异常无法处理，需⚠️。但@el.check中但403可以被处理
  * Created by cy on 2021/01/08.
  */
