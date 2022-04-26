bc05ee9564e453b88fdcc3eaf097cef3afde85a8
lWoHvYe <lWoHvYe@outlook.com>
2022-04-26 11:47:08 +0800

    无论是使用JDK还是CGLIB都能成功代理。鉴于通常都是CGLIB，这里保留JDK的方式

--- eladmin-starter/src/main/java/com/lwohvye/starter/modules/service/impl/TSServiceImpl.java
+++ eladmin-starter/src/main/java/com/lwohvye/starter/modules/service/impl/TSServiceImpl.java
@@ -26,7 +26,8 @@ import org.springframework.web.context.WebApplicationContext;
 @Service
 // 解决Bean链中某个Bean需要多例的问题
 // 使用session和request产生了一个新问题，生成controller的时候需要service作为controller的成员，但是service只在收到请求（可能是request也可能是session）时才会被实例化，controller拿不到service实例。
-// 为了解决这个问题，@Scope注解添加了一个proxyMode的属性，有两个值ScopedProxyMode.INTERFACES和ScopedProxyMode.TARGET_CLASS，前一个表示表示Service是一个接口，后一个表示Service是一个类。
+// 为了解决这个问题，@Scope注解添加了一个proxyMode的属性，有两个值ScopedProxyMode.INTERFACES和ScopedProxyMode.TARGET_CLASS，前一个表示表示Service是一个接口使用JDK动态代理模式，后一个表示Service是一个类使用CGLIB代理模式。
+// 比较有趣的是，这里用JDK或者CGLIB都可以，理论上虽然是实现了接口，注入的也是接口，但@Service是标记在类上的，所以一般/默认都是使用的CGLIB，但这里用JDK也能正常代理（且确实使用的JDK动态代理）
 @Scope(value = WebApplicationContext.SCOPE_REQUEST, proxyMode = ScopedProxyMode.INTERFACES)
 public class TSServiceImpl implements ITSService {
 
