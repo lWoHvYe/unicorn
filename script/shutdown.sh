f6679c9cf1159dcf69c17326b4432949f04790cf
lWoHvYe <lWoHvYe@outlook.com>
2022-04-30 13:43:37 +0800

    支持一次关闭所有服务，awk和shell还在学习中

--- script/shutdown.sh
+++ script/shutdown.sh
@@ -17,10 +17,11 @@
 
 echo "stop SpringBoot Application"
 # shellcheck disable=SC2009
-pid=$(ps -ef | grep eladmin-starter-3.0.3.jar | grep -v grep | awk '{print $2}')
-echo "旧应用进程id：$pid"
-if [ -n "$pid" ]
-then
+ps -ef | grep eladmin-starter-3.0.3.jar | grep -v grep | awk '{print $2}' | xargs kill -15
+#pid=$(ps -ef | grep eladmin-starter-3.0.3.jar | grep -v grep | awk '{print $2}')
+#echo "旧应用进程id：$pid"
+#if [ -n "$pid" ]
+#then
 # 通过使用-15 而不是-9 来停止线程
-kill -15 "$pid"
-fi
+#kill -15 "$pid"
+#fi
