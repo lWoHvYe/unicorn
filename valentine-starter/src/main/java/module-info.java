67c00319a5150d16be8e82a7a044ad21af8c6c51
王红岩 lWoHvYe <lwohvye@outlook.com>
2022-11-29 09:08:30 +0800

    fixup! try sync up with main branch

--- valentine-starter/src/main/java/module-info.java
+++ valentine-starter/src/main/java/module-info.java
@@ -7,8 +7,6 @@ module lwohvye.valentine.starter {
 
     exports com.unicorn.vs.rest to spring.beans, spring.aop, spring.web;
 
-    exports com.unicorn.vs.rest to spring.beans, spring.web;
-
     opens config; // 注意，resources目录下的子目标并没有被open，所以需要单独open，或者直接open整个module
     opens com.unicorn;
     opens com.unicorn.vs.rest to spring.core;
