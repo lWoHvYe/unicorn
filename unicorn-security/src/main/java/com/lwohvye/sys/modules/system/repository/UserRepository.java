bdc05bd3fbc479c8d3dcf81c5834d7ba6d9384c4
王红岩 lWoHvYe <lwohvye@outlook.com>
2023-02-16 22:20:45 +0800

    - fixed javadoc issue - modify some configurations

--- unicorn-security/src/main/java/com/lwohvye/sys/modules/system/repository/UserRepository.java
+++ unicorn-security/src/main/java/com/lwohvye/sys/modules/system/repository/UserRepository.java
@@ -48,11 +48,6 @@ public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificat
      * 若配置了Graph图，则查询会变成查满足条件的所有。然后内存分页。故分页类查询不建议使用。下面两个只作为存档。
      * 但一些根据特定条件只过滤出很少记录的，可以考虑使用，比如这里的根据id、name等的查询
      * 这个感觉跟MyBatis中配置的多对一的association标签，一对多的collection标签差不多，感觉很美好，但内存分页是硬伤
-     *
-     * @param spec
-     * @param pageable
-     * @return org.springframework.data.domain.Page
-     * @date 2021/11/9 10:05 下午
      */
     //@Override
     // @EntityGraph(value = "User-Details") // 11-26，这个加入意义不大，且对查询注解的Test造成mistake，故注释掉
