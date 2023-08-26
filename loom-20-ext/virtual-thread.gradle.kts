0a2d234fc584c56364a85bb3b8d54980f08f58ae
王红岩 lWoHvYe <lwohvye@outlook.com>
2023-08-26 11:17:41 +0800

    Kotlin DSL

--- loom-20-ext/virtual-thread.gradle.kts
+++
@@ -1,6 +0,0 @@
-import org.springframework.boot.gradle.plugin.SpringBootPlugin
-
-dependencies {
-    implementation(platform(SpringBootPlugin.BOM_COORDINATES))
-    implementation("org.springframework.boot:spring-boot-starter")
-}
