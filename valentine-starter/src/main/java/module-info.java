4419264db284132ab60c8355db27bf8084d22a1d
王红岩 lWoHvYe <lwohvye@outlook.com>
2023-08-26 15:02:01 +0800

    rollback some changes for Java 20

---
+++ valentine-starter/src/main/java/module-info.java
@@ -0,0 +1,20 @@
+@SuppressWarnings({"requires-automatic"})
+module lwohvye.valentine.starter {
+    requires lwohvye.unicorn.security;
+    requires lwohvye.unicorn.core;
+    requires lwohvye.unicorn.tp.tools;
+    requires lombok;
+    // 如果使用3rd-tools，需要加入下面这两个，不清楚为何在tools中加没生效。mail works well under unnamed module
+    requires jakarta.mail;
+    requires jakarta.activation;
+    requires spring.retry;
+    requires bizlog.sdk;
+    requires kotlin.stdlib;
+    requires kotlin.reflect;
+    requires kotlinx.coroutines.core;
+    requires org.apache.logging.log4j;
+    requires org.apache.httpcomponents.core5.httpcore5;
+    requires org.apache.httpcomponents.client5.httpclient5;
+
+    opens com.unicorn;
+}
