f672211cc08d96093e9f03890a4e598e103bc3b4
王红岩 lWoHvYe <lwohvye@outlook.com>
2023-09-09 09:54:33 +0800

    Revert preview disable

--- unicorn-tp-tools/unicorn-tp-tools-kotlin.gradle.kts
+++ unicorn-tp-tools/unicorn-tp-tools-kotlin.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.kotlin.spring)
     alias(libs.plugins.kotlin.jpa)
-//    alias(libs.plugins.kotlin.lombok)
+    alias(libs.plugins.kotlin.lombok)
 }
 
 description = "3rd工具模块"
