6c7fb910ad4c7dfbe3b39c42874a3a1ed13f3a59
王红岩 lWoHvYe <lwohvye@outlook.com>
2023-09-20 09:10:24 +0800

    [Draft] update shardingsphere to 5.4.0 no much db-config changes

--- unicorn-starter/src/main/java/module-info.java
+++ unicorn-starter/src/main/java/module-info.java
@@ -4,7 +4,7 @@ module lwohvye.unicorn.starter {
     requires lwohvye.unicorn.tp.tools;
     requires lwohvye.unicorn.code.gen;
     requires kotlin.stdlib;
-    requires kotlinx.coroutines.core.jvm;
+    requires kotlinx.coroutines.core;
     // 如果使用3rd-tools，需要加入下面这两个，不清楚为何在tools中加没生效
     requires jakarta.mail;
     requires jakarta.activation;
