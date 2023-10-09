adbabf8e8477479b9d94b1a10e558e40a8fcb77a
王红岩 lWoHvYe <lwohvye@outlook.com>
2023-10-09 13:16:46 +0800

    Add authorities as custom claims in JWT access tokens (userId & roles) (claim should not contain value with type Long)

--- unicorn-oauth2/samples-default-authorizationserver/src/main/java/sample/config/Oauth2TokenCustomizerConfig.java
+++ unicorn-oauth2/samples-default-authorizationserver/src/main/java/sample/config/Oauth2TokenCustomizerConfig.java
@@ -18,22 +18,37 @@ package sample.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.oauth2.core.oidc.OidcUserInfo;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.oauth2.core.oidc.endpoint.OidcParameterNames;
+import org.springframework.security.oauth2.server.authorization.OAuth2TokenType;
 import org.springframework.security.oauth2.server.authorization.token.JwtEncodingContext;
 import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenCustomizer;
 import sample.service.OidcUserInfoService;
 
+import java.util.Collections;
+import java.util.stream.Collectors;
+
 @Configuration
-public class IdTokenCustomizerConfig {
+public class Oauth2TokenCustomizerConfig {
 
     @Bean
     public OAuth2TokenCustomizer<JwtEncodingContext> tokenCustomizer(
             OidcUserInfoService userInfoService) {
         return (context) -> {
             if (OidcParameterNames.ID_TOKEN.equals(context.getTokenType().getValue())) {
-                OidcUserInfo userInfo = userInfoService.loadUser(context.getPrincipal().getName());
+                var userInfo = userInfoService.loadUser(context.getPrincipal().getName());
                 context.getClaims().claims(claims -> claims.putAll(userInfo.getClaims()));
+            } else if (OAuth2TokenType.ACCESS_TOKEN.equals(context.getTokenType())) {
+                var userInfo = userInfoService.loadUser(context.getPrincipal().getName());
+                var roles = AuthorityUtils.authorityListToSet(context.getPrincipal().getAuthorities())
+                        .stream()
+                        .map(c -> c.replaceFirst("^ROLE_", ""))
+                        .collect(Collectors.collectingAndThen(Collectors.toSet(), Collections::unmodifiableSet));
+                context.getClaims().claims(claims -> {
+                    var userInfoClaims = userInfo.getClaims();
+                    claims.put("userId", userInfoClaims.get("userId"));
+                    claims.put("roles", roles);
+                });
             }
         };
     }
