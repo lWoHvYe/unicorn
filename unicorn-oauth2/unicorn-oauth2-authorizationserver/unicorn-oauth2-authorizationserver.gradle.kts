e897fe04fc46fb3fee8d1b1ede3d0bd4f2b66047
王红岩 lWoHvYe <lwohvye@outlook.com>
2023-10-10 23:37:18 +0800

    Refactor Groovy to Kotlin DSL - part1

---
+++ unicorn-oauth2/unicorn-oauth2-authorizationserver/unicorn-oauth2-authorizationserver.gradle.kts
@@ -0,0 +1,18 @@
+import org.springframework.boot.gradle.plugin.SpringBootPlugin
+
+dependencies {
+    implementation(platform(SpringBootPlugin.BOM_COORDINATES))
+    implementation(libs.authorization.server)
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-cache")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
+    runtimeOnly("com.mysql:mysql-connector-j")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.security:spring-security-test")
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("net.sourceforge.htmlunit:htmlunit")
+}
---
+++ unicorn-oauth2/unicorn-oauth2-gateway-client/unicorn-oauth2-gateway-client.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    id("org.springframework.boot")
+    id("io.spring.dependency-management")
+}
+
+extra["springCloudVersion"] = project.findProperty("springCloudVersion") as String
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+    }
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+    implementation("org.springframework.cloud:spring-cloud-starter-gateway")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation("org.springframework.session:spring-session-data-redis")
+    implementation(libs.springdoc.webflux.ui)
+    implementation(libs.redisson)
+    implementation("org.thymeleaf.extras:thymeleaf-extras-springsecurity6")
+    implementation("org.webjars:webjars-locator-core")
+    implementation("org.webjars:bootstrap:5.2.3")
+    implementation("org.webjars:popper.js:2.9.3")
+    implementation("org.webjars:jquery:3.6.4")
+
+}
