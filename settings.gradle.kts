1bed4d317d0a24a68ec8e967a7f1b6deaa281ab1
Hongyan, Wang <39266195+lWoHvYe@users.noreply.github.com>
2024-02-22 09:20:24 +0800

    Qodana (#330)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -25,6 +25,7 @@ pluginManagement {
 
 plugins {
     id("com.gradle.enterprise") version "3.15"
+    id("org.gradle.toolchains.foojay-resolver-convention") version ("0.8.0")
 }
 
 dependencyResolutionManagement {
