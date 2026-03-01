01212f6a3fd3c7765739ebcea66b82d6e65abd02
王红岩 lWoHvYe <lwohvye@outlook.com>
2026-03-01 09:08:37 +0800

    mybatis

--- unicorn-oauth2/unicorn-mybatis/build.gradle.kts
+++ unicorn-oauth2/unicorn-mybatis/build.gradle.kts
@@ -19,11 +19,11 @@ plugins {
     id("io.spring.dependency-management")
 }
 
-extra["MyBatisPlusVersion"] = "3.5.12"
+extra["MyBatisPlusVersion"] = "3.5.15"
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-webmvc")
-    implementation("com.baomidou:mybatis-plus-spring-boot3-starter:${property("MyBatisPlusVersion")}")
+    implementation("com.baomidou:mybatis-plus-spring-boot4-starter:${property("MyBatisPlusVersion")}")
     implementation("com.baomidou:mybatis-plus-jsqlparser:${property("MyBatisPlusVersion")}")
     runtimeOnly("com.h2database:h2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
